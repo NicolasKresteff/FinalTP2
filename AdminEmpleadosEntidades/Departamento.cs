f2c6a9331411dde51732b2224ba9002ac47ee551
Facundo <facurey.arg@gmail.com>
2025-12-02 22:28:54 -0300

    que rica la meme

--- AdminEmpleadosEntidades/Departamento.cs
+++ AdminEmpleadosEntidades/Departamento.cs
@@ -1,10 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AdminEmpleadosEntidades
 {
-    internal class Departamento
+    public class Departamento
     {
+        [Key]
+        public int DepartamentoId { get; set; }
+
+        public int dpto_nro { get; set; }
+
+        public string? Nombre { get; set; }
     }
 }
--- AdminEmpleadosEntidades/Empleado.cs
+++ AdminEmpleadosEntidades/Empleado.cs
@@ -1,7 +1,41 @@
-﻿namespace AdminEmpleadosEntidades
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AdminEmpleadosEntidades
 {
     public class Empleado
     {
+        public int? EmpleadoId { get; set; }
+
+        public string? Dni { get; set; }
+
+        [MaxLength(100)]
+        public string? Nombre { get; set;  }
+
+        [MaxLength(500)]
+        public string? Direccion { get; set; }
+
+        public DateTime? FechaIngreso { get; set; }
+
+        [Column(TypeName ="decimal(8,2")]
+        public decimal? Salario { get; set; }
+
+        [ForeignKey("Departamento")] //EF (Object-Relational Mapping)
+        public int? dpto_id { get; set; }
+
+        public Departamento? Departamento { get; set; }
+
+        [NotMapped] //EF
+        public string? NombreDepartamento
+        {
+            get
+            {
+                if (Departamento != null)
+                    return Departamento.Nombre;
+                else
+                    return null;
+            }
+        }
 
     }
 }
